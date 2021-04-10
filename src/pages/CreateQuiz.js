29ba07174c09fe97ba358947e0f032c15eb5ecaa
Anmol Pant <anmolpant@gmail.com>
2021-04-10 22:31:40 +0530

    update createQuiz

--- src/pages/CreateQuiz.js
+++ src/pages/CreateQuiz.js
@@ -99,4 +99,117 @@ function CreateQuiz() {
 		}
 	}, []);
 
-	
+	if (loading) {
+		return <Loading />;
+	} else if (redirect) {
+		return <Redirect to="/dashboard" />;
+	} else if (redirectEdit) {
+		return <Redirect to={`/editQuiz/${quizId}`} />;
+	} else {
+		return (
+			<Container className="create-quiz-page">
+				<div className="create-form">
+					<Typography variant="h4" className="create-head">
+						Quiz Details
+					</Typography>
+					<div className="create-form-inputs">
+						<TextInput
+							variant="outlined"
+							label="Quiz Name"
+							value={quizName}
+							onChange={onQuizNameChange}
+							name="Quiz Name"
+							className="form-input"
+						/>
+
+						<MuiPickersUtilsProvider utils={DateFnsUtils}>
+							<Grid
+								className="date-time-select"
+								container
+								spacing={3}
+							>
+								<Grid item xs={12} sm={6}>
+									<KeyboardDatePicker
+										disableToolbar
+										variant="inline"
+										format="MM/dd/yyyy"
+										margin="normal"
+										label="Select Quiz Date"
+										value={quizDate}
+										onChange={handleDateChange}
+									/>
+								</Grid>
+								<Grid item xs={12} sm={6}>
+									<KeyboardTimePicker
+										ampm={true}
+										format="hh:mm:ss aa"
+										views={["hours", "minutes", "seconds"]}
+										margin="normal"
+										label="Select Quiz Start Time"
+										value={quizDate}
+										onChange={handleDateChange}
+										keyboardIcon={<AccessAlarm />}
+									/>
+								</Grid>
+							</Grid>
+						</MuiPickersUtilsProvider>
+						<p style={{ marginTop: "5%", marginBottom: "5%" }}>
+							Quiz Time (in minutes):
+						</p>
+						<Slider
+							defaultValue={5}
+							aria-labelledby="quiz time slider"
+							step={5}
+							min={5}
+							max={60}
+							valueLabelDisplay="on"
+							marks
+							className="time-slider"
+							value={duration}
+							onChange={handleTimeChange}
+						/>
+						<p>Select quiz type: </p>
+						<Select
+							value={type}
+							onChange={onTypeChange}
+							className="type-select"
+						>
+							<MenuItem value="public">Public</MenuItem>
+							<MenuItem value="private">Private</MenuItem>
+						</Select>
+
+						<Button
+							className="login-btn create-btn"
+							onClick={handleSubmit}
+						>
+							Create Quiz
+						</Button>
+						<Typography
+							variant="subtitle1"
+							className="create-subtitle"
+						>
+							NOTE: After creating the quiz, you can add questions
+							by editing the quiz in YOUR QUIZZES section of the
+							dashboard.
+						</Typography>
+					</div>
+				</div>
+				<Snackbar
+					open={error}
+					autoHideDuration={5000}
+					onClose={() => setError(false)}
+				>
+					<Alert
+						variant="filled"
+						severity="error"
+						onClose={() => setError(false)}
+					>
+						There was a problem. Please try again!
+					</Alert>
+				</Snackbar>
+			</Container>
+		);
+	}
+}
+
+export default CreateQuiz;
