6c1be5c45c09636a4c47e3a824ad41d414ec5e05
Anmol Pant <anmolpant@gmail.com>
2021-05-23 01:06:16 +0530

    create checkAuth

---
+++ Backend/api/middleware/checkAuth.js
@@ -0,0 +1,15 @@
+
+  if (!token) return res.status(400).send("Access Denied!, no token entered");
+
+  try {
+    const verified = JWT.verify(token, process.env.jwtSecret);
+
+    req.user = verified;
+    // console.log(verified);
+
+    next();
+    // console.log("next");
+  } catch (err) {
+    res.status(400).send({ error: "auth failed, check auth-token222" });
+  }
+};
