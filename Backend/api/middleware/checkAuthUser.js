04ddeea39fe20f4f2a3e189bc743ecdb4a73f2f9
Anmol Pant <anmolpant@gmail.com>
2021-05-25 12:03:05 +0530

    add catch(400)

--- Backend/api/middleware/checkAuthUser.js
+++ Backend/api/middleware/checkAuthUser.js
@@ -15,5 +15,7 @@ module.exports = function (req, res, next) {
         message: "not an User",
       });
     }
-  } 
+  } catch (err) {
+    res.status(400).send({ error: "auth failed, check auth-token" });
+  }
 };
