6b2b469cd3c9b7107bff222aaf609e5dc59de1fc
Anmol Pant <anmolpant@gmail.com>
2021-05-26 23:13:51 +0530

    email, mobile and password

--- Backend/emails/owner.js
+++ Backend/emails/owner.js
@@ -8,5 +8,21 @@ const ownerSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	name: { type: String },
 
+	userType: { type: String, default: "Owner" },
+	email: {
+		type: String,
+		lowercase: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+	},
+	mobileNumber: {
+		type: Number,
+		match: /^([7-9][0-9]{9})$/g,
+	},
+	password: { type: String },
+	boardPosition: { type: String },
+	token: {
+		type: String,
+	},
+});
 
 module.exports = mongoose.model("Owner", ownerSchema);
