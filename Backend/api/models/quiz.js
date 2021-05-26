18ec073daa006791214b4a92df77ad4fa9a939a5
Anmol Pant <anmolpant@gmail.com>
2021-05-26 23:27:59 +0530

    create quiz model

---
+++ Backend/api/models/quiz.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Admin = require("./admin");
+const Questions = require("./question");
+const User = require("./user");
+
+const QuizSchema = new mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	quizName: { type: String, required: true },
+	quizCode: { type: String },
+	adminId: { type: mongoose.Schema.Types.ObjectID, ref: "Admin" },
+	quizType: { type: String },
+	
+});
+
+module.exports = mongoose.model("Quiz", QuizSchema);
