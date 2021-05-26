ed2803c90f7b3e62b2b6aee3b46abb1a7a503fd9
Anmol Pant <anmolpant@gmail.com>
2021-05-26 23:06:19 +0530

    create owner model - imports

---
+++ Backend/api/models/owner.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const Quiz = require("./quiz");
+const Questions = require("./question");
+const Admin = require("./admin");
+const User = require("./user");
+
