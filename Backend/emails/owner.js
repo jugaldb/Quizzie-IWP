7aa6d9c24793c7c2b0d8f0d17f5d851d78c9828e
Anmol Pant <anmolpant@gmail.com>
2021-05-26 23:12:47 +0530

    create owner model - imports

--- Backend/emails/owner.js
+++ Backend/emails/owner.js
@@ -4,3 +4,9 @@ const Questions = require("./question");
 const Admin = require("./admin");
 const User = require("./user");
 
+const ownerSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	name: { type: String },
+
+
+module.exports = mongoose.model("Owner", ownerSchema);
